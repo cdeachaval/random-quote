f60c7638a8a832b4b718af0e4a2688647bd54d05
cdeachaval <christian_deachaval@mba.berkeley.edu>
2023-08-12 22:19:39 -0700

    second push

--- src/App.js
+++ src/App.js
@@ -27,10 +27,12 @@ function App() {
       <div id="quote-box">
         <p id="text">{quoteInfo.text}</p>
         <p id="author">{quoteInfo.author}</p>
+        <p id="new-quote"></p>
+        <a id="tweet-quote" href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text"> </a>
       </div>
       <div id="button-box">
-        <button id="new-quote" onClick={getQuote}>New Quote</button>
-        <a id="tweet-quote" 
+        <button id="the-quote" onClick={getQuote}>New Quote</button>
+        <a id="tweet-it" 
           href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
             + encodeURIComponent('"' + quoteInfo.text + '" ' + quoteInfo.author)}
           >Tweet</a>
