ed88d32c3038d5052f9cd636bab58f54577f87e5
cdeachaval <christian_deachaval@mba.berkeley.edu>
2023-08-12 22:10:38 -0700

    first push

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [quoteInfo, setQuoteInfo] = useState({})
+
+  useEffect(() => {
+    getQuote();
+  }, []);
+
+  const getQuote = () => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => { return response.json();
+      })
+      .then((data) => {
+        setQuoteInfo({
+          text: data.content,
+          author: data.author
+        });
+      });
+  }
+
+  return (
+    <div className="App">
+      <h1 class="app_title" >Random Quote:</h1>
+      <div id="quote-box">
+        <p id="text">{quoteInfo.text}</p>
+        <p id="author">{quoteInfo.author}</p>
+      </div>
+      <div id="button-box">
+        <button id="new-quote" onClick={getQuote}>New Quote</button>
+        <a id="tweet-quote" 
+          href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' 
+            + encodeURIComponent('"' + quoteInfo.text + '" ' + quoteInfo.author)}
+          >Tweet</a>
+      </div>  
+    </div>
+  );
+}
+
+export default App;
